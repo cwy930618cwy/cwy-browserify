77dacced65b175c2705ff6ad508544bf2de28cd2
cwy <834415728@qq.com>
2021-12-24 11:17:24 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('hhhh')
+
+var unique = require('uniq')
+
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6]
+
+console.log(unique(data))
+
+var add = require('cwy-number-add')
+
+var b = add(12, 2)
+
+console.log(b)
